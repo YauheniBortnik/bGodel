bcdc25a602455430aae6b6a393fd5d3b456d3032
Yauheni Bortnik <y.bortnik@godeltech.com>
2018-04-28 10:56:28 +0300

    Add files, change files

---
+++ form.php
@@ -0,0 +1,34 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+
+<body>
+<div id="orig_text_block">
+    <?php
+    $file = fopen('template.tpl', 'r');
+    $file = file('template.tpl');
+    $text = "";
+    foreach ($file as $value) {
+        echo $text = $value . "<br>";
+    }
+    ?>
+</div>
+
+<div id="form_block">
+    <form id="main_form" method="POST" action="index.php">
+        <p>Name:</p> <input type="text" name="name" class="input"/><br><br>
+        <p>Number:</p> <input type="number" name="number" class="input"/><br><br>
+        <p>Month:</p> <input type="number" name="month" class="input"/><br><br>
+        <input type="submit" value="Enter" name="change_btn" id="change_btn">
+    </form>
+</div>
+
+</body>
+
+</html>
+
+
+
+
--- index.php
+++ index.php
@@ -1,37 +1,55 @@
-
 <?php
-
-?>
-<html>
-
-<head>
-    <link rel="stylesheet" href="style.css" type="text/css"/>
-
-</head>
-
-<body>
-    <div id="orig_text_block">
-        <?php
-            echo "Original text:<br>";
-            echo file_get_contents('template.tpl');
-        ?>
-    </div>
-
-    <div id="form_block">
-        <form  id="main_form" method="POST" action="index.php">
-            <p>Name:</p> <input type="text" name="name" class="input"/><br><br>
-            <p>Number:</p> <input type="number" name="number" class="input"/><br><br>
-            <p>Month:</p> <input type="number" name="month" class="input"/><br><br>
-            <input type="submit" value="Enter" name="change_btn" id="change_btn">
-        </form>
-    </div>
-
-    <div id="res_text_block">
-        <?php
-            echo "Result text:<br>";
-            include ('check.php');
-        ?>
-    </div>
-</body>
-
-</html>
+$file = fopen('template.tpl', 'r');
+$file = file('template.tpl');
+if (isset($_POST['change_btn'])) {
+    $USERNAME = $_POST['name'];
+    $NUMBER = $_POST['number'];
+    $MONTHNUM = $_POST['month'];
+    if (empty($USERNAME)) {
+        echo '<script>alert("Please, enter NAME!!!");</script>';
+    }
+    if (empty($NUMBER)) {
+        echo '<script>alert("Please, enter NUMBER!!!");</script>';
+    }
+    if (empty($MONTHNUM)) {
+        echo '<script>alert("Please, enter MONTH!!!");</script>';
+    } else {
+        echo "<link rel='stylesheet' href='style.css'>";
+        $EXECDATE = date('d.m.Y');
+        $ENDDATE = date_create();
+        date_modify($ENDDATE, $MONTHNUM . "month");
+        $ENDDATE = date_format($ENDDATE, "d.m.Y");
+        $text = "";
+        foreach ($file as $value) {
+            $value = str_replace("%USERNAME%", "<span>" . $USERNAME . "</span>", $value);
+            $value = str_replace("%NUMBER%", "<span>" . $NUMBER . "</span>", $value);
+            $value = str_replace("%EXECDATE%", "<span>" . $EXECDATE . "</span>", $value);
+            $value = str_replace("%MONTHNUM%", "<span>" . $MONTHNUM . "</span>", $value);
+            $value = str_replace("%ENDDATE%", "<span>" . $ENDDATE . "</span>", $value);
+            echo $text = $value . "<br>";
+        }
+    }
+} elseif (isset($argv)) {
+    if (!empty($argv[1]) && !empty($argv[2]) && !empty($argv[3])) {
+        $USERNAME = $argv[1];
+        $NUMBER = $argv[2];
+        $MONTHNUM = $argv[3];
+        $ENDDATE = date('d.m.Y H:i');
+        $ENDDATE = date_create();
+        date_modify($ENDDATE, $MONTHNUM . "month");
+        $ENDDATE = date_format($ENDDATE, "d.m.Y");
+        $text = "";
+        foreach ($file as $value) {
+            $value = str_replace("%USERNAME%", $USERNAME, $value);
+            $value = str_replace("%NUMBER%", $NUMBER, $value);
+            $value = str_replace("%MONTHNUM%", $MONTHNUM, $value);
+            $value = str_replace("%EXECDATE%", $ENDDATE, $value);
+            $value = str_replace("%ENDDATE%", $ENDDATE, $value);
+            echo $text = $value;
+        }
+    } else {
+        echo $text = "Please, fill all fields!\n";
+    }
+} else {
+    require_once('form.php');
+}
