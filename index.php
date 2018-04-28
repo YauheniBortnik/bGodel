beb0ec6c71a034cf8012c942a09a21a17e12c2cf
Yauheni Bortnik <y.bortnik@godeltech.com>
2018-04-28 14:41:18 +0300

    Improve some isssues

--- index.php
+++ index.php
@@ -2,53 +2,66 @@
 $file = fopen('template.tpl', 'r');
 $file = file('template.tpl');
 if (isset($_POST['change_btn'])) {
-    $USERNAME = $_POST['name'];
-    $NUMBER = $_POST['number'];
-    $MONTHNUM = $_POST['month'];
-    if (empty($USERNAME)) {
+    echo "<link rel='stylesheet' href='style.css'>";
+    $userName = $_POST['name'];
+    $number = $_POST['number'];
+    $monthNum = $_POST['month'];
+    if (empty($userName)) {
+        require_once('form.php');
         echo '<script>alert("Please, enter NAME!!!");</script>';
+    } elseif (preg_match('/^([0-9a-zA-Z-_.]{5,16})$/', $userName)) {
+        require_once('form.php');
+        echo '<script>alert("Please, enter NAME only with LETTERS!!!");</script>';
     }
-    if (empty($NUMBER)) {
+    if (empty($number)) {
         echo '<script>alert("Please, enter NUMBER!!!");</script>';
+        require_once('form.php');
     }
-    if (empty($MONTHNUM)) {
+    if (empty($monthNum)) {
         echo '<script>alert("Please, enter MONTH!!!");</script>';
+        require_once('form.php');
     } else {
-        echo "<link rel='stylesheet' href='style.css'>";
-        $EXECDATE = date('d.m.Y');
-        $ENDDATE = date_create();
-        date_modify($ENDDATE, $MONTHNUM . "month");
-        $ENDDATE = date_format($ENDDATE, "d.m.Y");
+        require_once('form.php');
+        $execDate = date('d.m.Y');
+        $endDate = date_create();
+        date_modify($endDate, $monthNum . "month");
+        $endDate = date_format($endDate, "d.m.Y");
         $text = "";
-        foreach ($file as $value) {
-            $value = str_replace("%USERNAME%", "<span>" . $USERNAME . "</span>", $value);
-            $value = str_replace("%NUMBER%", "<span>" . $NUMBER . "</span>", $value);
-            $value = str_replace("%EXECDATE%", "<span>" . $EXECDATE . "</span>", $value);
-            $value = str_replace("%MONTHNUM%", "<span>" . $MONTHNUM . "</span>", $value);
-            $value = str_replace("%ENDDATE%", "<span>" . $ENDDATE . "</span>", $value);
-            echo $text = $value . "<br>";
-        }
+        ?>
+        <div id="res_text_block">
+            <?php
+            foreach ($file as $value) {
+                $value = str_replace("%USERNAME%", "<span>" . $userName . "</span>", $value);
+                $value = str_replace("%NUMBER%", "<span>" . $number . "</span>", $value);
+                $value = str_replace("%EXECDATE%", "<span>" . $execDate . "</span>", $value);
+                $value = str_replace("%MONTHNUM%", "<span>" . $monthNum . "</span>", $value);
+                $value = str_replace("%ENDDATE%", "<span>" . $endDate . "</span>", $value);
+                echo $text = $value . "<br>";
+            }
+            ?>
+        </div>
+        <?php
     }
 } elseif (isset($argv)) {
-    if (!empty($argv[1]) && !empty($argv[2]) && !empty($argv[3])) {
-        $USERNAME = $argv[1];
-        $NUMBER = $argv[2];
-        $MONTHNUM = $argv[3];
-        $ENDDATE = date('d.m.Y H:i');
-        $ENDDATE = date_create();
-        date_modify($ENDDATE, $MONTHNUM . "month");
-        $ENDDATE = date_format($ENDDATE, "d.m.Y");
+    if (!empty($argv[1]) && (preg_match('/^([0-9a-zA-Z-_.]{5,16})$/', $argv[1])) && !empty($argv[2]) && !empty($argv[3])) {
+        $userName = $argv[1];
+        $number = $argv[2];
+        $monthNum = $argv[3];
+        $execDate = date('d.m.Y H:i');
+        $endDate = date_create();
+        date_modify($endDate, $monthNum . "month");
+        $endDate = date_format($endDate, "d.m.Y");
         $text = "";
         foreach ($file as $value) {
-            $value = str_replace("%USERNAME%", $USERNAME, $value);
-            $value = str_replace("%NUMBER%", $NUMBER, $value);
-            $value = str_replace("%MONTHNUM%", $MONTHNUM, $value);
-            $value = str_replace("%EXECDATE%", $ENDDATE, $value);
-            $value = str_replace("%ENDDATE%", $ENDDATE, $value);
+            $value = str_replace("%USERNAME%", $userName, $value);
+            $value = str_replace("%NUMBER%", $number, $value);
+            $value = str_replace("%MONTHNUM%", $monthNum, $value);
+            $value = str_replace("%EXECDATE%", $execDate, $value);
+            $value = str_replace("%ENDDATE%", $endDate, $value);
             echo $text = $value;
         }
     } else {
-        echo $text = "Please, fill all fields!\n";
+        echo $text = "Please, fill ALL fields or(and) fill firs arg in LETTERS!\n";
     }
 } else {
     require_once('form.php');
